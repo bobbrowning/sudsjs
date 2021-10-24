5848444b04e8f6f7065204a0fb4d7a72672bc619
Bob Browning <75942393+bobbrowning@users.noreply.github.com>
2021-10-24 11:10:07 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Please enter the app name"
+read appName
+pwd
+
+mv suds-express-demo-main appName
+rm suds-express-demo-main.zip
+cd ./$appName
+pwd
+npm install 
+echo "SUDS Installed"
+echo "1. cd $appName"
+echo "2. In your browser http://localhost:3000/admin"
+echo "3. log in with admin@admin.com, password admin"
