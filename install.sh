116cec157bf4f13c2105f9b94302f0ce28da1aa0
Bob Browning <75942393+bobbrowning@users.noreply.github.com>
2022-08-31 11:50:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,22 +3,20 @@ echo "Please enter the app name"
 read appName
 echo "MongoDB test database(M) or SQLite(S) "
 read db
+echo db
 mv sudsjs-main $appName
 cd ./$appName
 pwd
 
-if [ db != 'S' ]
-then
-    mongorestore --drop
-    echo "MongoDB database restored"
-fi
-if [ db = 'S' ]
-then
+if [ db = 'S' ]; then
     mv tables tables.mongo
     mv cong conf.mongo
     mv tables.sql tables
     mv conf.sql conf
     echo "Usimng SQLite3 Database"
+else
+    mongorestore --drop
+    echo "MongoDB database restored"
 fi
 npm install
 echo "SUDS Installed - starting up"
