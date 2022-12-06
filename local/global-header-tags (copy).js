2cdc6146bebad99c146690d7878050430cb8ecff
Bob Browning <75942393+bobbrowning@users.noreply.github.com>
2022-12-06 12:14:48 +0000

    CouchDB version

---
+++ local/global-header-tags (copy).js
@@ -0,0 +1,12 @@
+
+module.exports =`
+ <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BLF835FV2V"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BLF835FV2V');
+</script> 
+ `;
---
+++ local/global-header-tags.js
@@ -0,0 +1,4 @@
+
+module.exports =`
+<!-- local global header tags -->
+ `;
