955c71895a649f1c8a11bb4a181e085cb569d24a
Bob Browning <75942393+bobbrowning@users.noreply.github.com>
2023-08-17 12:17:27 +0100

    Minor update

--- backups/mysqldump.sql
+++ backups/mysqldump.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: suds
 -- ------------------------------------------------------
--- Server version	8.0.33-0ubuntu0.20.04.2
+-- Server version	8.0.33-0ubuntu0.20.04.4
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -742,4 +742,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-26 10:33:54
+-- Dump completed on 2023-08-17 12:14:28
